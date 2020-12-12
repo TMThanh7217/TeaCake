6e1c826ce71accd5ad477d9ea803bef662402aff
TMThanh7217 <onthidaihocvatly12@gmail.com>
2020-12-12 21:33:17 +0700

    Update register layout

--- public/js/login_register.js
+++ public/js/login_register.js
@@ -6,6 +6,8 @@ var formbox = document.getElementById("form-box");
 var buttonbox = document.getElementById("button-box");
 var bg = document.getElementById("background");
 
+var login_btn = document.getElementById("login-btn");
+login_btn.onclick = 
 function login(){
     loginform.style.left = "50px";
     registerform.style.left = "450px";
@@ -15,23 +17,18 @@ function login(){
     TnK_img.style.display = "inline";
     buttonbox.style.boxShadow = "0 0 30px 20px #3623631f";
     bg.style.backgroundImage = "linear-gradient(rgba(38, 207, 212, 0.4),rgba(214, 141, 30, 0.2)),url(../images/tea_cake_bg.jpg)";
-    var tmp = document.getElementById("login-btn");
-    tmp.style.outline = "none";
-    tmp.style.border = "0";
-}
+};
 
-function register(){
+var register_btn = document.getElementById("register-btn");
+register_btn.onclick = function register(){
     loginform.style.left = "-400px";
     registerform.style.left = "50px";
     button.style.left = "100px";
     button.style.background = "linear-gradient(to right, #f5dacd, #f56623)";
     formbox.style.borderColor = "#fa823c";
     TnK_img.style.display = "none";
-    buttonbox.style.boxShadow = "0 0 30px 20px #ff24241f";
+    buttonbox.style.boxShadow = "0 0 30px 0px #ff24241f";
     bg.style.backgroundImage = "linear-gradient(rgba(214, 141, 30, 0.2), rgba(38, 207, 212, 0.4)),url(../images/tea_cake_bg.jpg)";
-    var tmp = document.getElementById("register-btn");
-    tmp.style.outline = "none";
-    tmp.style.border = "0";
 }
 
 // Source: https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username
