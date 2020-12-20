c7d2da78feaa647d04180ea3d169a9d69cc4d324
Tran Phuoc Loc <giongnhuntlf@gmail.com>
2020-12-20 13:51:12 +0700

    reroute add ads + credit + cart + history + profile + contact + blogs page

---
+++ routes/blog.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var myModules = require('../myModules/array')
+var path = require('path');
+var fs = require('fs');
+var blogs = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../public/json/blogs.json')));
+
+router.get('/', (req, res) => { // credit page
+    // ---- get user
+    res.locals.user = req.app.get('current_user');
+
+    var rows = myModules.getRows(blogs, 3);
+    // ---- Prepare data for page
+    var page_data = {
+      title: "TeaCake - Blog",
+      rows: rows
+    }
+
+    // ---- Render home page
+    res.render('blog', page_data);
+});
+
+module.exports = router;
